1f698dc9c2c9ded2479fdeef7be5233b03923d37
Lisa Smith <lisa@simplymeasured.com>
2014-04-12 22:52:40 -0700

    Updates to get this working on the app engine

--- ponies/pony.go
+++ ponies/pony.go
@@ -12,6 +12,7 @@ import (
 
 func init() {
     http.HandleFunc("/", handler)
+    http.HandleFunc("/handleEmo", handleEmo)
 }
 type Stable struct{
     Faces []Pony
@@ -22,28 +23,35 @@ type Pony struct{
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprint(w, ponyForm)
-    if(len(r.FormValue("emotion")) > 0){//only request/show ponies when an emotion was entered
-        c := appengine.NewContext(r)
-        client := urlfetch.Client(c)
-        resp, _ := client.Get("http://ponyfac.es/api.json/tag:" + r.FormValue("emotion"))
+}
 
-        body, _ := ioutil.ReadAll(resp.Body)
-        stable := new(Stable)
-        json.Unmarshal(body, stable)
+func handleEmo(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    client := urlfetch.Client(c)
+    resp, _ := client.Get("http://ponyfac.es/api.json/tag:" + r.FormValue("emotion"))
 
-        for i := 0; i < len(stable.Faces); i++ {
-            fmt.Fprintf(w, "<img src=\"%v\"/>", stable.Faces[i].Image)
-        }
+    body, _ := ioutil.ReadAll(resp.Body)
+    stable := new(Stable)
+    json.Unmarshal(body, stable)
+    
+    fmt.Fprint(w, ponyForm)
+    fmt.Fprintf(w, "<html>")
+
+    for i := 0; i < len(stable.Faces); i++ {
+        fmt.Fprintf(w, "<img src=\"%v\"/>", stable.Faces[i].Image)
     }
+    fmt.Fprintf(w, "</html>")
 }
 
 const ponyForm = `
 <html>
+<head>
+<title>Emo Ponies</title>
+</head>
   <body>
-    <form action="/handler" method="post">
+    <form action="/handleEmo" method="post">
       <div id="entry-form">
-        <label for="emotion">Emotion:</label><input name="emotion" rows="3" cols="60">
-        </input><input type="submit" value="Get Ponies">
+        <label for="emotion">Emotion:</label><input name="emotion" rows="3" cols="60"/><input type="submit" value="Get Ponies">
         <div>
             <a href="http://www.cornify.com" onclick="cornify_add();return false;"><img src="http://www.cornify.com/assets/cornifycorn.gif" width="52" height="51" border="0" alt="Cornify" /></a><script type="text/javascript" src="http://www.cornify.com/js/cornify.js"></script>
         </div>
